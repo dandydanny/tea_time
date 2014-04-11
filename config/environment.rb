ac7c7c88a67cdda85c2d683b27815d5227f2eec7
Daniel Lu <johndoe@example.com>
2014-04-11 18:11:01 -0500

    Remove rspec from environment

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ require "sinatra/reloader" if development?
 require 'erb'
 require 'bcrypt'
 require 'json'
-require 'rspec'
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
